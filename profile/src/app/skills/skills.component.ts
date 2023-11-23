6d990a1687f9dd2fd43fc9f59d7aea192f3324e7
Afrikhana <brianmurigi64@gmail.com>
2023-11-23 09:37:53 +0300

    :rocket:Deploying first development

--- profile/src/app/skills/skills.component.ts
+++ profile/src/app/skills/skills.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit , OnDestroy, ChangeDetectorRef} from '@angular/core';
 
 @Component({
   selector: 'app-skills',
@@ -10,6 +10,14 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
       <p>The skills I have attained from <br>internships, school and personal <br>
           development in software development are as follows.</p>
     </div>
+    <div class="count">
+        <p>Yr:{{ years }}</p>
+        <p>M:{{ months }}</p>
+        <p>D:{{ days }}</p>
+        <p>Hrs:{{ hours }}</p>
+        <p id='min'>Min:{{ minutes }}</p>
+        <p id='sec'>Sec:{{ seconds }}</p>
+      </div>
     <div class="content2">
       <!-- frontennt content -->
       <div id="front">
@@ -61,7 +69,7 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   styleUrls: ['./skills.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class SkillsComponent {
+export class SkillsComponent implements OnInit, OnDestroy{
   public flask: string ='https://firebasestorage.googleapis.com/v0/b/profile-7bfa2.appspot.com/o/flask.png?alt=media&token=3c9111d7-edbd-463c-a1f3-be911e2bd9ef'
   public css: string = 'https://firebasestorage.googleapis.com/v0/b/profile-7bfa2.appspot.com/o/css.png?alt=media&token=26b1a58d-8bd7-4e31-97f7-67847e07a3e0'
   public html: string = 'https://firebasestorage.googleapis.com/v0/b/profile-7bfa2.appspot.com/o/html5.jpg?alt=media&token=68e450ce-1f27-497a-85be-b4efe34cdff4'
@@ -71,4 +79,48 @@ export class SkillsComponent {
  public react:string = 'https://firebasestorage.googleapis.com/v0/b/profile-7bfa2.appspot.com/o/react.jpg?alt=media&token=d7c8fa1c-ee82-4d63-8f87-b513222bf711';
  public python:string = 'https://firebasestorage.googleapis.com/v0/b/profile-7bfa2.appspot.com/o/python.jpg?alt=media&token=9411ed44-d2fc-42ce-a17c-79dabccc233f';
  public mysql: string = 'https://firebasestorage.googleapis.com/v0/b/profile-7bfa2.appspot.com/o/sql-libraries.jpg?alt=media&token=eb2dfcbe-7a30-4ff1-b5d0-838f33eee02e';
+     
+ years: number = 0;
+ months: number = 0;
+ days: number = 0;
+ hours: number = 0;
+ minutes: number = 0;
+ seconds: number = 0;
+ countdownInterval: any;
+
+ constructor(private changeDetectorRef: ChangeDetectorRef) {}
+
+ ngOnInit() {
+   const startDate = new Date('2023-11-01T00:00:00');
+   this.updateCountdown(startDate);
+
+   this.countdownInterval = setInterval(() => {
+     this.updateCountdown(startDate);
+     this.changeDetectorRef.detectChanges(); // Trigger change detection
+   }, 1000);
+ }
+
+ updateCountdown(startDate: Date): void {
+   const currentDate = new Date();
+   const timeDifference = currentDate.getTime() - startDate.getTime();
+
+   this.seconds = Math.floor((timeDifference / 1000) % 60);
+   this.minutes = Math.floor((timeDifference / (1000 * 60)) % 60);
+   this.hours = Math.floor((timeDifference / (1000 * 60 * 60)) % 24);
+   this.days = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
+   this.months = Math.floor(this.days / 30);
+   this.years = Math.floor(this.months / 12);
+ }
+
+ ngOnDestroy() {
+   if (this.countdownInterval) {
+     clearInterval(this.countdownInterval);
+   }
+ }
+   
+
+
 }
+
+
+
